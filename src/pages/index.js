986cd7b9646329eca8f6005cbfdefef06751cd3a
Ivan Cheban <ivancheban@gmail.com>
2020-08-11 17:38:10 +0300

    button

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,7 +68,7 @@ function Home() {
           <div className={styles.buttons}>
             <Link
               className={clsx(
-                'button button--outline button--lg',
+                'button button--outline button--primary--lg',
                 styles.getStarted,
               )}
               to={useBaseUrl('docs/')}>
